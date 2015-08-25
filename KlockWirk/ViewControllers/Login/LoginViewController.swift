d7a237f75947e8929c42f1bcfc876a15d125849b
Casey Egan <casey.egan@fcsamerica.com>
2015-08-24 19:15:02 -0500

    Misc

--- KlockWirk/ViewControllers/Login/LoginViewController.swift
+++ KlockWirk/ViewControllers/Login/LoginViewController.swift
@@ -11,20 +11,17 @@ import UIKit
 class LoginViewController: UIViewController {
     
     
-    let loginService = LoginService()
-    let klockWirkService = KlockWirkServices()
+    let loginService        = LoginService()
+    let klockWirkService    = KlockWirkServices()
     
     @IBOutlet weak var emaiAddress: UITextField!
     @IBOutlet weak var password: UITextField!
 
-    override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        
-        registerNotification()
-    }
     
     
+    
+
+    //MARK: Notifications
     
     func registerNotification(){
         
@@ -54,24 +51,11 @@ class LoginViewController: UIViewController {
         )
     }
     
-    
     func retrieveKlockWirkersCompeleted(notification: NSNotification){
     
         storeKlockWirkers(notification.userInfo as! Dictionary<String, NSArray>)
         loadMerchantTabBarController()
     }
-    
-    
-    func loginFailed(){
-        
-        
-        let alertController = UIAlertController(title: "Login", message:
-            "Login Failed", preferredStyle: UIAlertControllerStyle.Alert)
-        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
-        
-        self.presentViewController(alertController, animated: true, completion: nil)
-    }
-    
     
     func loginWasSuccessful(notification: NSNotification){
         
@@ -92,17 +76,30 @@ class LoginViewController: UIViewController {
         if(isMerchant == true){
             
             ApplicationInformation.setMerchantId((loginInfo!.objectForKey("MerchantId") as? Int!)!)
-        
+            
             klockWirkService.getAllKlockWirkers(ApplicationInformation.getMerchantId())
         }
     }
     
+    func loginFailed(){
+        
+        
+        let alertController = UIAlertController(title: "Login", message:
+            "Login Failed", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    //MARK: Utility Methods
     
     func storeKlockWirkers(klockWirkers: Dictionary<String, NSArray>){
         
         ApplicationInformation.setKlockWirkers(JSONUtilities.parseKlockWirkers(klockWirkers[Keys.KlockWirkersKey]!))
     }
-    
     
     func loadMerchantTabBarController(){
         
@@ -113,6 +110,22 @@ class LoginViewController: UIViewController {
     }
     
 
+    
+    
+    //MARK: View Delegates
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        registerNotification()
+    }
+    
+    
+    
+    
+    //MARK: Actions
+    
     @IBAction func login(sender: AnyObject) {
         
         loginService.login(emaiAddress.text!, password: password.text!)
