d87977a5ca781eb6d021f4e8d5b74484cd283fdb
Casey Egan <casey.egan@fcsamerica.com>
2015-09-08 19:48:26 -0500

    Refactoring account setup views

--- KlockWirk/ViewControllers/AccountSetup/KlockWirkerSetupViewController.swift
+++ KlockWirk/ViewControllers/AccountSetup/KlockWirkerSetupViewController.swift
@@ -1,47 +1,51 @@
 //
-//  KlockWirkerSetupViewController.swift
+//  KlockWirkerSetupViewController2ViewController.swift
 //  KlockWirk
 //
-//  Created by Casey Egan on 8/14/15.
-//  Copyright (c) 2015 KlockWirk. All rights reserved.
+//  Created by Casey Egan on 9/7/15.
+//  Copyright © 2015 KlockWirk. All rights reserved.
 //
 
 import UIKit
 
-class KlockWirkerSetupViewController: UIViewController {
-
+class KlockWirkerSetupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
     let klockWirkService = KlockWirkerServices()
     
-    @IBOutlet weak var emailAddress: UITextField!
-    @IBOutlet weak var phoneNumber: UITextField!
-    @IBOutlet weak var password: UITextField!
-    @IBOutlet weak var confirmPassword: UITextField!
+    var klockWirkerRegistrationFields = NSMutableArray()
+    var klockWirkerLoginFields = NSMutableArray()
+    
+    @IBOutlet weak var klockWirkerSetupTableView: UITableView!
     
     
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
+        
+        klockWirkerRegistrationFields = getKlockWirkerRegistrationFields()
+        klockWirkerLoginFields = getKlockWirkerLoginFields()
+        
+        
+        registerNotification()
         setupViewProperties()
         setupNavigationButtons()
-        registerNotification()
-    }
-
-   
-    func setupViewProperties(){
         
-        self.title = "Registration"
+        setupTableViewDelegates()
+        setupTableViewProperties()
     }
     
-    func setupNavigationButtons(){
+    
+    func doesKlockWirkerPasswordsMatch() -> Bool{
         
-        let submit = UIBarButtonItem(title: "Submit", style: UIBarButtonItemStyle.Plain, target: self, action: "submitButtonTapped")
-        let cancel = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "cancelButtonTapped")
+        let kw = getCompletedKlockWirkerRegistration()
         
-        self.navigationItem.leftBarButtonItem = cancel
-        self.navigationItem.rightBarButtonItem = submit
+        if(kw.password != kw.confirmPassword){
+            
+            return false
+        }
+        
+        return true
     }
     
     
@@ -57,7 +61,6 @@ class KlockWirkerSetupViewController: UIViewController {
         )
     }
     
-    
     func registerKlockWirkerCompeleted(){
         
         let tabBarController:KlockWirkTabBarController = KlockWirkTabBarController()
@@ -66,15 +69,219 @@ class KlockWirkerSetupViewController: UIViewController {
     }
     
     
+    func setupViewProperties(){
+        
+        self.title = "Registration"
+    }
+    
+    func setupNavigationButtons(){
+        
+        let submit = UIBarButtonItem(title: "Submit", style: UIBarButtonItemStyle.Plain, target: self, action: "submitButtonTapped")
+        let cancel = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "cancelButtonTapped")
+        
+        self.navigationItem.leftBarButtonItem = cancel
+        self.navigationItem.rightBarButtonItem = submit
+    }
+    
+    
+    func getKlockWirkerRegistrationFields() -> NSMutableArray{
+        
+        let klockWirkerRegistrationFields = NSMutableArray()
+        
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "Email", val: "", tag: 1))
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "Phone", val: "", tag: 2))
+        
+        return klockWirkerRegistrationFields
+    }
+    
+    
+    func getKlockWirkerLoginFields() -> NSMutableArray{
+        
+        let klockWirkerRegistrationFields = NSMutableArray()
+        
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "Password", val: "", tag: 1))
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "Confirm Password", val: "", tag: 1))
+       
+        return klockWirkerRegistrationFields
+    }
+    
+    
+    func getCompletedKlockWirkerRegistration() -> KlockWirker{
+        
+        let klockWirker = KlockWirker()
+        
+        let emailAddress = klockWirkerSetupTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)) as! EnrollmentViewCell
+        let phone = klockWirkerSetupTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 0)) as! EnrollmentViewCell
+        let password = klockWirkerSetupTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 1)) as! EnrollmentViewCell
+        let confirmPassword = klockWirkerSetupTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 1)) as! EnrollmentViewCell
+        
+        klockWirker.emailAddress = emailAddress.enrollmentTextField.text!
+        klockWirker.phoneNumber = phone.enrollmentTextField.text!
+        klockWirker.password = password.enrollmentTextField.text!
+        klockWirker.confirmPassword = confirmPassword.enrollmentTextField.text!
+        
+        return klockWirker
+    }
+    
+    
+    func displayAlert(message: String){
+        
+        let alertController = UIAlertController(title: "Setup", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+
+    
+    
     func submitButtonTapped(){
         
-        //klockWirkService.registerKlockWirker(emailAddress.text!, phoneNumber: phoneNumber.text!, password: password.text!)
+        if(doesKlockWirkerPasswordsMatch()){
+            
+            klockWirkService.registerKlockWirker(getCompletedKlockWirkerRegistration()) { (response: NSDictionary) in
+                
+                let statusCode = (response.objectForKey("statusCode") as? Int)!
+                
+                if(statusCode == HTTPStatusCodes.HTTPNotFound){
+                    
+                    self.displayAlert("Not Found")
+                }
+                else{
+                    
+                    let tabBarController:KlockWirkTabBarController = KlockWirkTabBarController()
+                    
+                    self.navigationController?.pushViewController(tabBarController, animated: false)
+                }
+            }
+        }
+        else{
+            
+            displayAlert("Password mismatch")
+        }
     }
     
     func cancelButtonTapped(){
-     
+        
         self.dismissViewControllerAnimated(true, completion: nil)
         
     }
+    
+    
+    func setupTableViewProperties(){
+        
+        klockWirkerSetupTableView.separatorStyle = UITableViewCellSeparatorStyle.None
+    }
+    
+    func setupTableViewDelegates(){
+        
+        klockWirkerSetupTableView.delegate = self
+        klockWirkerSetupTableView.dataSource = self
+        klockWirkerSetupTableView.registerNib(UINib(nibName: "EnrollmentViewCell", bundle: nil), forCellReuseIdentifier: "enrollmentTableViewCell")
+    }
+    
+    
+    
+    
+    //MARK: TableView Delegates
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 2
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if(section == 0){
+            
+            return klockWirkerRegistrationFields.count
+        }
+        else{
+            
+            return klockWirkerLoginFields.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if(section == 0) {
+            
+            let view = UIView()
+            let label = UILabel()
+            
+            label.text = "Enter the email address and phone number you received in your registration email"
+            label.textColor = UIColor.lightGrayColor()
+        
+            label.font = UIFont (name: "HelveticaNeue-LightItalic", size: 14)
+            
+            
+            label.numberOfLines = 3
+            label.frame = CGRectMake(20, 10, 330, 75)
+         
+            view.addSubview(label)
+       
+            return view
+        }
+        else if(section == 1){
+            
+            let view = UIView() // The width will be the same as the cell, and the height should be set in tableView:heightForRowAtIndexPath:
+            let label = UILabel()
+            label.text="Create you password"
+            label.textColor = UIColor.lightGrayColor()
+            label.font = UIFont (name: "HelveticaNeue-LightItalic", size: 14)
+            label.frame = CGRectMake(20, 10, 330, 50)
+         
+            view.addSubview(label)
+           
+            return view
+            
+        }
+        return nil
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        switch (section) {
+            
+            case 0:
+                return 75
+            
+        case 1:
+            return 50
+            
+        default:
+            return 50
+    
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var accountField = AccountSetupField()
+        let cell:EnrollmentViewCell = klockWirkerSetupTableView.dequeueReusableCellWithIdentifier("enrollmentTableViewCell") as! EnrollmentViewCell
+        
+        switch (indexPath.section) {
+            
+        case 0:
+            accountField = klockWirkerRegistrationFields.objectAtIndex(indexPath.row) as! AccountSetupField
+            
+        case 1:
+            accountField = klockWirkerLoginFields.objectAtIndex(indexPath.row) as! AccountSetupField
+            
+        default:
+            cell.textLabel?.text = "Other"
+        }
+        
+        cell.bindCellDetail(accountField)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
+    }
+
 
 }
--- KlockWirk/ViewControllers/ManageKlockWirkers/NewKlockWirkerViewController.swift
+++ KlockWirk/ViewControllers/ManageKlockWirkers/NewKlockWirkerViewController.swift
@@ -1,33 +1,75 @@
 //
-//  NewKlockWirkerViewController.swift
+//  NewKlockWirkerViewController2.swift
 //  KlockWirk
 //
-//  Created by Casey Egan on 8/15/15.
-//  Copyright (c) 2015 KlockWirk. All rights reserved.
+//  Created by Casey Egan on 9/8/15.
+//  Copyright © 2015 KlockWirk. All rights reserved.
 //
 
 import UIKit
 
-class NewKlockWirkerViewController: UIViewController {
+class NewKlockWirkerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate   {
 
     
     let klockWirkService = KlockWirkerServices()
-   
-    
-    @IBOutlet weak var firstName: UITextField!
-    @IBOutlet weak var lastName: UITextField!
-    @IBOutlet weak var emailAddress: UITextField!
-    @IBOutlet weak var phone: UITextField!
-    
+    var klockWirkerRegistrationFields = NSMutableArray()
+    @IBOutlet weak var newKlockWirkerTableView: UITableView!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
+        
         setupViewProperties()
         setupNavigationButtons()
+        setupTableViewDelegates()
+        setupTableViewProperties()
+        
+        klockWirkerRegistrationFields = getKlockWirkerRegistrationFields()
     }
     
+    
+    func getKlockWirkerRegistrationFields() -> NSMutableArray{
+        
+        let klockWirkerRegistrationFields = NSMutableArray()
+        
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "First Name", val: "", tag: 1))
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "Last Name", val: "", tag: 2))
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "Email Address", val: "", tag: 3))
+        klockWirkerRegistrationFields.addObject(AccountSetupField(lbl: "Phone", val: "", tag: 4))
+        
+        return klockWirkerRegistrationFields
+    }
+    
+    func getCompletedKlockWirkerRegistration() -> KlockWirker{
+        
+        let klockWirker = KlockWirker()
+        
+        let firstName = newKlockWirkerTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)) as! EnrollmentViewCell
+        let lastName = newKlockWirkerTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 0)) as! EnrollmentViewCell
+        let emailAddress = newKlockWirkerTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 2, inSection: 0)) as! EnrollmentViewCell
+        let phoneNumber = newKlockWirkerTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 3, inSection: 0)) as! EnrollmentViewCell
+        
+        klockWirker.firstName = firstName.enrollmentTextField.text!
+        klockWirker.lastName = lastName.enrollmentTextField.text!
+        klockWirker.emailAddress = emailAddress.enrollmentTextField.text!
+        klockWirker.phoneNumber = phoneNumber.enrollmentTextField.text!
+        klockWirker.password = ""
+        
+        return klockWirker
+    }
+    
+    
+    func setupTableViewProperties(){
+        
+        newKlockWirkerTableView.separatorStyle = UITableViewCellSeparatorStyle.None
+    }
+    
+    func setupTableViewDelegates(){
+        
+        newKlockWirkerTableView.delegate = self
+        newKlockWirkerTableView.dataSource = self
+        newKlockWirkerTableView.registerNib(UINib(nibName: "EnrollmentViewCell", bundle: nil), forCellReuseIdentifier: "enrollmentTableViewCell")
+    }
     
     func setupNavigationButtons(){
         
@@ -37,17 +79,13 @@ class NewKlockWirkerViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = cancel
         self.navigationItem.rightBarButtonItem = submit
     }
-    
 
-    
     func submitButtonTapped(){
-
+        
         let loadingNotification = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
         loadingNotification.mode = MBProgressHUDMode.Indeterminate
-        
-        let kw = KlockWirker(firstName: firstName.text!, lastName: lastName.text!, emailAddress: emailAddress.text!, phoneNumber: phone.text!, password: "")
-        
-        klockWirkService.addNewKlockWirker(kw) { (response: NSDictionary) in
+    
+        klockWirkService.addNewKlockWirker(getCompletedKlockWirkerRegistration()) { (response: NSDictionary) in
             
             ApplicationInformation.addKlockWirker(JSONUtilities.parseKlockWirker(response))
             
@@ -55,16 +93,74 @@ class NewKlockWirkerViewController: UIViewController {
         }
     }
     
-    
     func cancelButtonTapped(){
         
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    
-    
     func setupViewProperties(){
         
         self.navigationItem.title = "Add KlockWirker"
+    }
+    
+    
+    
+    
+    //MARK: TableView Delegates
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return klockWirkerRegistrationFields.count
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if(section == 0) {
+            
+            let view = UIView()
+            let label = UILabel()
+            
+            label.text = "Enter the below information to setup a new KlockWirker"
+            label.textColor = UIColor.lightGrayColor()
+            
+            label.font = UIFont (name: "HelveticaNeue-LightItalic", size: 14)
+            
+            
+            label.numberOfLines = 2
+            label.frame = CGRectMake(20, 10, 330, 30)
+            
+            view.addSubview(label)
+            
+            return view
+        }
+       
+        return nil
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+
+        let accountField = klockWirkerRegistrationFields.objectAtIndex(indexPath.row) as! AccountSetupField
+        let cell:EnrollmentViewCell = newKlockWirkerTableView.dequeueReusableCellWithIdentifier("enrollmentTableViewCell") as! EnrollmentViewCell
+       
+        cell.bindCellDetail(accountField)
+        
+        return cell
+  
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
     }
 }
